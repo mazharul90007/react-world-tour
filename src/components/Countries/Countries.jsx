53805bf8365054e88f4902bb65bbf7a18a1e4b66
mazharul90007 <mazharul90007@gmail.com>
2024-09-05 18:23:22 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css"
+
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([]);
+
+    useEffect(() => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+
+    const handleVisitedCountry = country => {
+        console.log('add this to your visited country');
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
+
+    const handleVisitedFlags = flag => {
+        console.log(flag)
+        const newVisitedFlag = [...visitedFlags, flag];
+        setVisitedFlags(newVisitedFlag);
+    }
+    return (
+        <div>
+            <h3>Countries: {countries.length}</h3>
+            {/* visited countries */}
+            <div>
+                <h5>Visited Countries: {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+            {/* Display flag */}
+            <div className="flag-container">
+                    {
+                        visitedFlags.map((flag, idx) => <img key={idx} src={flag}></img>)
+                    }
+            </div>
+            {/* display country */}
+            <div className="country-container">
+                {
+                    countries.map(country => <Country key={country.cca3} handleVisitedCountry={handleVisitedCountry} handleVisitedFlags={handleVisitedFlags} country={country}></Country>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
